2ef9ac36e7cb3c53fca082e8a44655f94cd64492
GunjanSingh1 <gunjansingh1203@gmail.com>
2020-05-22 02:48:30 +0530

    .

---
+++ Experiments/abox.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# all reasoner commands
+
+#echo "**************OWL2Bench2 EL*********************"
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 1 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 2 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 5 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 10 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 20 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 50 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 100 EL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 200 EL 1 >trash.log
+
+#echo "**************OWL2Bench2 QL*********************"
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 1 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 2 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 5 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 10 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 20 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 50 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 100 QL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 200 QL 1 >trash.log
+
+#echo "**************OWL2Bench2 RL*********************"
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 1 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 2 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 5 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 10 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 20 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 50 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 100 RL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 200 RL 1 >trash.log
+
+#echo "**************OWL2Bench2 DL*********************"
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 1 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 2 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 5 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 10 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 20 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 50 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 100 DL 1 >trash.log
+java -Xms24g -Xmx24g -jar OWL2Bench.jar 200 DL 1 >trash.log
